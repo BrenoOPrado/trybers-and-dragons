5effea0be4837f982e22aa4904d5a7e4514b977d
BrenoOPrado <breno.o.prado@hotmail.com>
2022-11-18 19:05:54 -0300

    Interface Fighter criada

--- src/Archetypes/Necromancer.ts
+++ src/Archetypes/Necromancer.ts
@@ -1,5 +1,5 @@
-import { EnergyType } from "../Energy";
-import Archetype from "./Archetype";
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
 
 export default class Necromancer
   extends Archetype {
